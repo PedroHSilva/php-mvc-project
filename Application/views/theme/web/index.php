4223fd71980730acf80c860c9b1187eae4659833
pedrohsilva <pedro_silva@id.uff.br>
2021-06-22 21:31:06 -0300

    add view index.php

--- Application/views/theme/web/index.php
+++ Application/views/theme/web/index.php
@@ -1,7 +1,5 @@
-<div class="container">
-  <div class="row">
-    <div class="col-8 offset-2 text-center" style="margin-top:150px">
-      <h1>Construindo um simples Framework MVC com PHP <?= url() ?></h1>
-    </div>
+<div class="w3-display-container" style="width:100vw; height:100vh">
+  <div class="w3-display-middle w3-center">
+    <h1 class="w3-padding w3-card">Seja bem vindo ao PHP MVC Project :)</h1>
   </div>
 </div>
